7506148bd59d16800899c22196dbb930fe26135a
Thiago Assi <thiago-assi@outlook.com>
2021-12-14 02:08:07 -0300

    created main AI

--- src/com/letscode/tictactoe/Main.java
+++ src/com/letscode/tictactoe/Main.java
@@ -8,6 +8,11 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         Player player = drawPlayer();
+        Player computer;
+
+        if (player == Player.X) computer = Player.O;
+        else computer = Player.X;
+
         char[] board = new char[9];
         Scanner scanner = new Scanner(System.in);
         String response;
@@ -43,13 +48,33 @@ public class Main {
                 }
                 else {
                     board = makeMove(board, responseNumber, player);
+                    String result = checkIfItsOver(board);
+                    if (!result.equals("")) {
+                        clearConsole();
+                        displayBoard(board);
+
+                        System.out.println("Vencedor: " + result);
+                        break;
+                    }
+
                     clearConsole();
-                    System.out.println("Conferindo...");
+                    System.out.println("É a vez do computador...");
                     delay(3, 700);
+
+                    int move = calculateMove(board, player);
+                    board = makeMove(board, move + 1, computer);
+                    result = checkIfItsOver(board);
+                    if (!result.equals("")) {
+                        clearConsole();
+                        displayBoard(board);
+
+                        System.out.println("Vencedor: " + result);
+                        break;
+                    }
+
                 }
             }
         }
-
 
         System.exit(0);
     }
@@ -70,6 +95,93 @@ public class Main {
 
         board[position - 1] = symbol;
         return board;
+    }
+
+    static String checkIfItsOver(char[] board) {
+        for (int i = 0; i < 9; i += 3) {
+            if (board[i] == board[1 + i] && board[i] == board[2 + i]) {
+                return String.valueOf(board[i]);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i] == board[3 + i] && board[i] == board[6 + i]) {
+                return String.valueOf(board[i]);
+            }
+        }
+
+        if (board[0] == board[4] && board[0] == board[8]) {
+            return String.valueOf(board[0]);
+        }
+
+        if (board[2] == board[4] && board[2] == board[6]) {
+            return String.valueOf(board[2]);
+        }
+
+        return "";
+    }
+
+    static int calculateMove(char[] board, Player player) {
+        char playerSymbol, computerSymbol;
+
+        if (player == Player.X) {
+            playerSymbol = 'X';
+            computerSymbol = 'O';
+        }
+        else {
+            playerSymbol = 'O';
+            computerSymbol = 'X';
+        }
+
+        for (int i = 0; i < 9; i += 3) {
+            if (board[i] == playerSymbol && board[1 + i] == playerSymbol && board[2 + i] != computerSymbol) {
+                return 2 + i;
+            }
+            if (board[i] == playerSymbol && board[2 + i] == playerSymbol && board[1 + i] != computerSymbol) {
+                return 1 + i;
+            }
+            if (board[1 + i] == playerSymbol && board[2 + i] == playerSymbol && board[i] != computerSymbol) {
+                return i;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i] == playerSymbol && board[3 + i] == playerSymbol && board[6 + i] != computerSymbol) {
+                return 6 + i;
+            }
+            if (board[i] == playerSymbol && board[6 + i] == playerSymbol && board[3 + i] != computerSymbol) {
+                return 3 + i;
+            }
+            if (board[3 + i] == playerSymbol && board[6 + i] == playerSymbol && board[i] != computerSymbol) {
+                return i;
+            }
+        }
+
+        if (board[0] == playerSymbol && board[4] == playerSymbol && board[8] != computerSymbol) {
+            return 8;
+        }
+        if (board[0] == playerSymbol && board[8] == playerSymbol && board[4] != computerSymbol) {
+            return 4;
+        }
+        if (board[4] == playerSymbol && board[8] == playerSymbol && board[0] != computerSymbol) {
+            return 0;
+        }
+
+        if (board[2] == playerSymbol && board[4] == playerSymbol && board[6] != computerSymbol) {
+            return 6;
+        }
+        if (board[2] == playerSymbol && board[6] == playerSymbol && board[4] != computerSymbol) {
+            return 4;
+        }
+        if (board[4] == playerSymbol && board[6] == playerSymbol && board[2] != computerSymbol) {
+            return 2;
+        }
+
+        for(int i = 0; i < 9; i++) {
+            if (board[i] != computerSymbol && board[i] != playerSymbol) return i;
+        }
+
+        return 0;
     }
 
     static void clearBoard(char[] board, Display display) {
